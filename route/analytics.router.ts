25c843098dca88af0f6ac04c3dbcf1a3fcdb28a7
SatwikVats <vats.satwik@gmail.com>
2024-01-12 22:41:33 +0530

    Added an endpointt for time-based analytics.

--- route/analytics.router.ts
+++ route/analytics.router.ts
@@ -2,8 +2,11 @@ import express from "express";
 const router = express.Router();
 
 import { verifyUser } from "../middleware/verifyUser";
-import { getAnalyticsHandler } from "../controller/analyticsHandler";
+import { getAnalyticsHandler, getTimeBasedAnalyticsHandler } from "../controller/analyticsHandler";
+
 
 router.route('/:shortURL/:userId').get(verifyUser, getAnalyticsHandler);
+router.route('/time/:shortURL/:userId').get(verifyUser, getTimeBasedAnalyticsHandler);
+
 
 export default router;
